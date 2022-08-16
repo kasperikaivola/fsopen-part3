950b42539cf90dd04eccb153de3e86723eadeb68
kasperikaivola <98049597+kasperikaivola@users.noreply.github.com>
2022-08-16 18:04:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const port = process.env.PORT
+const port = process.env.PORT || 3001
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
